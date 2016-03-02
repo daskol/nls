923a08e9ed40032d3249c8dbfddfce8282d13172
Daniel Bershatsky <daniel.bershatsky@skolkovotech.ru>
2016-03-02 21:29:55 +0300

    Add new features and iImprove CLI.

--- examples/visualize.py
+++ examples/visualize.py
@@ -22,19 +22,28 @@ from nls.model import Solution
 
 
 def main():
-    parser = ArgumentParser(description='Visualize saved solutions.')
+    parser = ArgumentParser(prog='NLSe Soliution Visualizer', description='Visualize saved solutions with nls package.')
     parser.add_argument('solution_path', metavar='solution', type=unicode, help='.mat file that contains solution')
-    parser.add_argument('--1d', '-1', action='store_true')
-    parser.add_argument('--2d', '-2', action='store_true')
-    parser.add_argument('--default', action='store_true')
-    parser.add_argument('--contour', action='store_true')
-    parser.add_argument('--stream', action='store_true')
+    parser.add_argument('--1d', '-1', action='store_true', help='Force loading solution as one dimensional')
+    parser.add_argument('--2d', '-2', action='store_true', help='Force loading solution as two dimensional')
+    parser.add_argument('--default', action='store_true', help='Visualize solution in default way')
+    parser.add_argument('--contour', action='store_true', help='Show contour plot')
+    parser.add_argument('--stream', action='store_true', help='Show stream plot')
+    parser.add_argument('--filename', '-f', metavar='path', default=None, type=unicode, help='Filename to save figure')
+    parser.add_argument('--show', '-s', action='store_true', help='Show visualized solution.')
+    parser.add_argument('--no-show', '-ns', action='store_true', help='Do not show visualized solution.')
 
     args = parser.parse_args()
 
     solution = Solution.load(args.solution_path)
-    solution.visualize()
-    solution.show()
+    solution.visualize(
+        filename = args.filename,
+        contour = args.contour,
+        stream = args.stream,
+        )
+    
+    if not args.no_show:
+        solution.show()
 
 
 if __name__ == '__main__':
